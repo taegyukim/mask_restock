fd6599f43254797ace6e26ad7a1696341b73a64e
Taegyu Kim <dev.tgkim@gmail.com>
2020-03-06 23:29:46 +0900

    파일 읽기/쓰기 로직 작성

--- test.ts
+++ test.ts
@@ -1,1 +1,16 @@
-console.log(1+2)
+var http = require('http');
+var fs = require('fs');
+
+// let rawData = fs.readFileSync('./smartstore-1.json');
+// let data = JSON.parse(rawData);
+const fileName = './smartstore-1.json';
+let data = require(fileName)
+if(1) {
+    data.status = true;
+    fs.writeFile(fileName, JSON.stringify(data, null, 2), (err: any) => {
+        if (err) throw err;
+        console.log(JSON.stringify(data));
+        console.log('writing to ' + fileName);
+    });
+}
+console.log(data);
